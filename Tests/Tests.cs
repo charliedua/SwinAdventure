dfa3827484ddb8f31651bdad1cba56ac6f0846ff
Anmol Dua <duaanmol2@gmail.com>
2018-07-27 11:48:39 +1000

    Added Tests

---
+++ Tests/Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace SwinAdventure.UnitTests.Services
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
