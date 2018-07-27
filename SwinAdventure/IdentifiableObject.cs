97af3390cf04bc90830936829d993c42d172c742
Anmol Dua <duaanmol2@gmail.com>
2018-07-27 10:51:59 +1000

    made identifiableobject

---
+++ SwinAdventure/IdentifiableObject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwinAdventure
+{
+    class IdentifiableObject
+    {
+        private List<string> _identifiers;
+
+        string FirstId
+        {
+            get
+            {
+                if (_identifiers.Count == 0)
+                {
+                    return "";
+                }
+                return _identifiers.First();
+            }
+        }
+
+        public IdentifiableObject(string[] idents)
+        {
+            _identifiers = idents.ToList();
+        }
+
+        public void AddIdentiﬁer(string id)
+        {
+            _identifiers.ForEach(delegate (string item)
+            {
+                item.ToLower();
+            });
+        }
+
+        public bool AreYou(string id)
+        {
+            return _identifiers.Contains(id);
+        }
+    }
+}
