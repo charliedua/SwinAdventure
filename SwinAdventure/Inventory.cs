c3642a9131c59d9f7afc61a3e2637897ea0dc29e
Anmol Dua <duaanmol2@gmail.com>
2018-07-29 23:23:00 +1000

    Made the skeleton From UML

---
+++ SwinAdventure/Inventory.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace SwinAdventure
+{
+    public class Inventory
+    {
+        private List<Item> _items;
+
+        public string ItemList
+        {
+            get
+            {
+                return _items.ToString();
+            }
+        }
+        public Inventory()
+        {
+
+        }
+
+        public bool HasItem(string id)
+        {
+            return false;
+        }
+
+        public void Put(Item itm)
+        {
+
+        }
+
+        public Item Take()
+        {
+
+        }
+
+        public Item Fetch()
+        {
+            
+        }
+    }
+}
--- SwinAdventure/Player.cs
+++ SwinAdventure/Player.cs
@@ -2,8 +2,28 @@
 {
     public class Player : GameObject
     {
+        private Inventory _inventory;
+
+        public Inventory Inventory
+        {
+            get { return _inventory; }
+        }
+
+        public override string FullDescription
+        {
+            get
+            {
+                return base.FullDescription;
+            }
+        }
+
         public Player(string name, string desc) : 
             base(new string[] { "me", "inventory"}, name, desc)
+        {
+
+        }
+
+        public GameObject Locate(string id)
         {
 
         }
